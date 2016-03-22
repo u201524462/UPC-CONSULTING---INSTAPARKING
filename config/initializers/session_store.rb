4b5b8e6153a515a23f471719965aa6324e240669
u201524476 <luis.canalest@gmail.com>
2016-03-22 03:03:14 +0000

    ContactUs Map - Done

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_insta-parking-sprint4_session'
+Rails.application.config.session_store :cookie_store, key: '_insta-parking-sprint5_session'
